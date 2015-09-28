02f902d4f8b988bdfda022845a0bf843d5051348
anniekummari <bujji@Bujjis-MBP.default>
2015-09-28 01:25:26 +0100

    Config
    Added config.java

---
+++ app/src/main/java/com/pixelmags/android/comms/Config.java
@@ -0,0 +1,21 @@
+package com.pixelmags.android.comms;
+
+/**
+ * Created by Annie on 28/09/15.
+ */
+public class Config
+{
+    public static final String API_BASE_URL = "https://api.pixelmags.com";
+    public static final String Support_EMail = "support.kindle@pixelmags.com";
+    public static final int Magazine_Number = 2;
+    public static final String Magazine_Title = "Now";
+    public static final String Bundle_ID = "om.pixelmags.reader.managed.android-amazon.ipc-media-now";
+    public static final String Version = "1.0";
+    public static final String Pixelmags_Website = "http://www.pixelmags.com";
+    public static final String Pixelmags_TermsConditions = "http://pixelmags.com/t+c/current";
+    public static final Boolean Is_WhiteLabel = false;
+    public static final Boolean Is_Dedicated = true;
+    public static final Boolean Is_Preflight = false;
+
+
+}
