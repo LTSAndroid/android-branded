c2950fe854d47e79c75afdd7ea4da69eb3bfaa95
anniekummari <bujji@Bujjis-MBP.default>
2015-10-10 02:44:58 +0530

    Added GetDocumentKey and Parser

---
+++ app/src/main/java/com/pixelmags/android/json/GetDocumentKeyParser.java
@@ -0,0 +1,34 @@
+package com.pixelmags.android.json;
+
+/**
+ * Created by Annie on 09/10/15.
+ */
+public class GetDocumentKeyParser extends JSONParser {
+
+    public Object mData;
+
+
+    public GetDocumentKeyParser(String Data){
+        super(Data);
+    }
+
+    public boolean parse(){
+
+        if(!initJSONParse())
+            return false; // return false if the JSON base object cannot be parsed
+
+        try{
+
+            mData = baseJSON.getJSONArray("data");
+
+        }catch(Exception e){}
+
+
+        return true;
+    }
+
+}
+
+
+
+
