b74e26b3f184b1906128e5d2f82703ca8f6d8957
austincoutinho <austin.coutinho@pixelmags.com>
2015-10-10 06:56:45 +0100

    Object models Single issue, issue catalog

--- app/src/main/java/com/pixelmags/android/datamodels/SingleIssue.java
+++ app/src/main/java/com/pixelmags/android/datamodels/SingleIssue.java
@@ -3,11 +3,10 @@ package com.pixelmags.android.datamodels;
 /**
  * Created by austincoutinho on 10/10/15.
  *
- * Model to store the data for each issue that is displayed in GetIssues
+ * Object model for a single issue within a Catalog of Issues
  *
  */
-public class IssuesCatalog {
-
+public class SingleIssue {
 
 
 
