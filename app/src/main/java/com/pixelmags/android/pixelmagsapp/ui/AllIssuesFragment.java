0f8cdf023921d5dd70c57c9e1a2c3c66ca240ee9
austincoutinho <austin.coutinho@pixelmags.com>
2015-10-24 01:35:47 +0100

    Custom GridView using BaseAdapter

--- app/src/main/java/com/pixelmags/android/pixelmagsapp/ui/AllIssuesFragment.java
+++ app/src/main/java/com/pixelmags/android/pixelmagsapp/ui/AllIssuesFragment.java
@@ -1,6 +1,7 @@
 package com.pixelmags.android.pixelmagsapp.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,6 +10,9 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.pixelmags.android.api.CreateUser;
 import com.pixelmags.android.api.GetIssues;
@@ -19,14 +23,42 @@ import com.pixelmags.android.json.GetIssuesParser;
 import com.pixelmags.android.json.JSONParser;
 import com.pixelmags.android.pixelmagsapp.R;
 import com.pixelmags.android.pixelmagsapp.test.ResultsFragment;
+import com.pixelmags.android.storage.AllIssuesDataSet;
 import com.pixelmags.android.storage.UserPrefs;
+import com.pixelmags.android.util.BaseApp;
 
 import java.util.ArrayList;
 
 
 public class AllIssuesFragment extends Fragment {
 
-
+    private Integer[] mThumbIds = {
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+            R.drawable.cast_ic_notification_0,
+    };
 
     private GetAllIssuesTask mGetAllIssuesTask = null;
 
@@ -40,18 +72,81 @@ public class AllIssuesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
+        View rootView = inflater.inflate(R.layout.fragment_display_issues, container, false);
+
         // retrieving the issues
         mGetAllIssuesTask = new GetAllIssuesTask(Config.Magazine_Number,Config.Bundle_ID);
         mGetAllIssuesTask.execute((String) null);
 
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.displayIssuesGridView);
+
+        gridview.setAdapter(new MyAdapter(getActivity()));
+        gridview.setNumColumns(4);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_issues, container, false);
+        return rootView;
+    }
+
+/**
+ *  A custom GridView to display the Magazines.
+ *
+     */
+    public class MyAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        public MyAdapter(Context c) {
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            return mThumbIds[arg0];
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View grid;
+
+            if(convertView==null){
+
+                grid = new View(mContext);
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                grid=inflater.inflate(R.layout.all_issues_custom_grid_layout, parent, false);
+
+            }else{
+                grid = (View)convertView;
+            }
+
+            ImageView imageView = (ImageView)grid.findViewById(R.id.gridImage);
+            imageView.setImageResource(mThumbIds[position]);
+
+            return grid;
+        }
     }
 
 
+
+
+
     /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
+     *
+     * Represents an asynchronous task used to fetch all the issues.
+     *
      */
     public class GetAllIssuesTask extends AsyncTask<String, String, String> {
 
@@ -74,7 +169,11 @@ public class AllIssuesFragment extends Fragment {
             try{
                 GetIssues apiGetIssues = new GetIssues();
                 apiGetIssues.init(mMagazineID,mAppBundleID);
-                resultToDisplay = apiGetIssues.getAPIResultData();
+
+                AllIssuesDataSet mDbHelper = new AllIssuesDataSet(BaseApp.getContext());
+                issuessArray = mDbHelper.getAllIssues(mDbHelper.getReadableDatabase());
+                mDbHelper.close();
+
 
             }catch (Exception e){
 
@@ -86,6 +185,7 @@ public class AllIssuesFragment extends Fragment {
         protected void onPostExecute(String result) {
 
             mGetAllIssuesTask = null;
+
 
            /* for(int i=0; i< issuessArray.size();i++) {
 
@@ -100,5 +200,6 @@ public class AllIssuesFragment extends Fragment {
         protected void onCancelled() {
             mGetAllIssuesTask = null;
         }
+
     }
 }
