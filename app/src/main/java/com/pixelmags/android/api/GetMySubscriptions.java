e07fe0dba4e0fb575a5817ea9342616123ec27b5
austincoutinho <austin.coutinho@pixelmags.com>
2015-10-31 03:00:32 +0000

    GetMySubscriptions api complete, tested

--- app/src/main/java/com/pixelmags/android/api/GetMySubscriptions.java
+++ app/src/main/java/com/pixelmags/android/api/GetMySubscriptions.java
@@ -1,10 +1,13 @@
 package com.pixelmags.android.api;
 
+import com.pixelmags.android.comms.Config;
 import com.pixelmags.android.comms.WebRequest;
+import com.pixelmags.android.datamodels.MySubscription;
 import com.pixelmags.android.datamodels.Subscription;
 import com.pixelmags.android.json.GetMySubscriptionsParser;
 import com.pixelmags.android.json.GetSubscriptionsParser;
 import com.pixelmags.android.storage.SubscriptionsDataSet;
+import com.pixelmags.android.storage.UserPrefs;
 import com.pixelmags.android.util.BaseApp;
 
 import org.apache.http.NameValuePair;
@@ -19,7 +22,7 @@ public class GetMySubscriptions extends WebRequest {
 
     private static final String API_NAME="getMySubscriptions";
 
-    GetMySubscriptionsParser mySubsParser;
+    public GetMySubscriptionsParser mySubsParser;
 
 
     public GetMySubscriptions(){
@@ -51,17 +54,31 @@ public class GetMySubscriptions extends WebRequest {
 
     private void setApiNameValuePairs(){
 
-        baseApiNameValuePairs = new ArrayList<NameValuePair>(4);
-        baseApiNameValuePairs.add(new BasicNameValuePair("magazine_id", baseMagazineId));
-        baseApiNameValuePairs.add(new BasicNameValuePair("app_bundle_id", baseAppBundleId));
-        baseApiNameValuePairs.add(new BasicNameValuePair("api_mode",baseApiMode));
-        baseApiNameValuePairs.add(new BasicNameValuePair("api_version", baseApiVersion));
+        baseApiNameValuePairs = new ArrayList<NameValuePair>(6);
+        baseApiNameValuePairs.add(new BasicNameValuePair("auth_email_address", UserPrefs.getUserEmail()));
+        baseApiNameValuePairs.add(new BasicNameValuePair("auth_password", UserPrefs.getUserPassword()));
+        baseApiNameValuePairs.add(new BasicNameValuePair("device_id", UserPrefs.getDeviceID()));
+        baseApiNameValuePairs.add(new BasicNameValuePair("magazine_id", Config.Magazine_Number));
+        baseApiNameValuePairs.add(new BasicNameValuePair("app_bundle_id", Config.Bundle_ID));
+        baseApiNameValuePairs.add(new BasicNameValuePair("api_mode", Config.api_mode));
+        baseApiNameValuePairs.add(new BasicNameValuePair("api_version", Config.api_version));
 
     }
 
     public void saveMySubscriptionsDataToApp(){
 
-        // Save the Subscription Objects into the SQlite DB
+
+        for(int i=0; i< mySubsParser.mySubscriptionsList.size();i++) {
+
+            MySubscription sub = mySubsParser.mySubscriptionsList.get(i);
+
+            System.out.println(" MySubscription CREDITS ::" + sub.creditsAvailable);
+        }
+
+
+        // Save the MySubscription Objects into the SQlite DB
+
+
         /*
         SubscriptionsDataSet mDbHelper = new SubscriptionsDataSet(BaseApp.getContext());
         mDbHelper.insert_all_subscriptions(mDbHelper.getWritableDatabase(), subsParser.subscriptionsList);
