d7fa94d2564c2e4e9ea20571cc7d81ac3c118dce
austincoutinho <austin.coutinho@pixelmags.com>
2015-10-31 03:00:04 +0000

    GetMyIssues api complete, tested

--- app/src/main/java/com/pixelmags/android/api/GetMyIssues.java
+++ app/src/main/java/com/pixelmags/android/api/GetMyIssues.java
@@ -1,8 +1,11 @@
 package com.pixelmags.android.api;
 
+import com.pixelmags.android.comms.Config;
 import com.pixelmags.android.comms.WebRequest;
+import com.pixelmags.android.datamodels.MyIssue;
 import com.pixelmags.android.json.GetIssuesParser;
 import com.pixelmags.android.json.GetMyIssuesParser;
+import com.pixelmags.android.storage.UserPrefs;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -15,23 +18,15 @@ import java.util.ArrayList;
 public class GetMyIssues extends WebRequest
 {
     private static final String API_NAME="getMyIssues";
-    private String mEmail;
-    private String mPassword;
-    private String mMagazineID;
-    private String mAppBundleID;
-    private String mDeviceID;
+
     GetMyIssuesParser getMyIssuesParser;
 
     public GetMyIssues(){
         super(API_NAME);
     }
-    public void init(String email, String password, String deviceID, String magazineID,String appBundleID)
+
+    public void init()
     {
-        mMagazineID = magazineID;
-        mAppBundleID = appBundleID;
-        mEmail = email;
-        mPassword = password;
-        mDeviceID = deviceID;
 
         setApiNameValuePairs();
         doPostRequest();
@@ -42,7 +37,7 @@ public class GetMyIssues extends WebRequest
 
                 if(getMyIssuesParser.isSuccess()){
                     getMyIssuesParser.parse();
-
+                    saveMyIssuesDataToApp();
                 } else{
 
                     // Add error handling code here
@@ -56,14 +51,35 @@ public class GetMyIssues extends WebRequest
     private void setApiNameValuePairs(){
 
         baseApiNameValuePairs = new ArrayList<NameValuePair>(7);
-        baseApiNameValuePairs.add(new BasicNameValuePair("email", mEmail));
-        baseApiNameValuePairs.add(new BasicNameValuePair("password", mPassword));
-        baseApiNameValuePairs.add(new BasicNameValuePair("device_id", mDeviceID));
-        baseApiNameValuePairs.add(new BasicNameValuePair("magazine_id", mMagazineID));
-        baseApiNameValuePairs.add(new BasicNameValuePair("app_bundle_id", mAppBundleID));
-        baseApiNameValuePairs.add(new BasicNameValuePair("api_mode", baseApiMode));
-        baseApiNameValuePairs.add(new BasicNameValuePair("api_version", baseApiVersion));
+        baseApiNameValuePairs.add(new BasicNameValuePair("auth_email_address", UserPrefs.getUserEmail()));
+        baseApiNameValuePairs.add(new BasicNameValuePair("auth_password", UserPrefs.getUserPassword()));
+        baseApiNameValuePairs.add(new BasicNameValuePair("device_id", UserPrefs.getDeviceID()));
+        baseApiNameValuePairs.add(new BasicNameValuePair("magazine_id", Config.Magazine_Number));
+        baseApiNameValuePairs.add(new BasicNameValuePair("app_bundle_id", Config.Bundle_ID));
+        baseApiNameValuePairs.add(new BasicNameValuePair("api_mode", Config.api_mode));
+        baseApiNameValuePairs.add(new BasicNameValuePair("api_version", Config.api_version));
 
     }
+
+    public void saveMyIssuesDataToApp(){
+
+        for(int i=0; i< getMyIssuesParser.myIssuesList.size();i++) {
+
+            MyIssue issue = getMyIssuesParser.myIssuesList.get(i);
+
+            System.out.println(" MyIssue ID ::" +issue.issueID);
+
+        }
+
+
+        // Save the Subscription Objects into the SQlite DB
+        /*
+        SubscriptionsDataSet mDbHelper = new SubscriptionsDataSet(BaseApp.getContext());
+        mDbHelper.insert_all_subscriptions(mDbHelper.getWritableDatabase(), subsParser.subscriptionsList);
+        mDbHelper.close();
+        */
+    }
+
+
 }
 
