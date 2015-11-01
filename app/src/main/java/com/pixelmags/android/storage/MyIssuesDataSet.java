4812ccdfc7b252c44593ad6c8063a87cf9e0d768
austincoutinho <austin.coutinho@pixelmags.com>
2015-11-01 02:48:06 +0000

    MyIssues - SQLite base

---
+++ app/src/main/java/com/pixelmags/android/storage/MyIssuesDataSet.java
@@ -0,0 +1,18 @@
+package com.pixelmags.android.storage;
+
+import android.content.Context;
+
+/**
+ * Created by austincoutinho on 01/11/15.
+ */
+public class MyIssuesDataSet  extends BrandedSQLiteHelper {
+
+
+    public MyIssuesDataSet(Context context) {
+        super(context);
+    }
+
+
+
+
+}
