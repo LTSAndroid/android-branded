eedfe7b10d5d0a226f830c99c57e7d8a5fd2e277
austincoutinho <austin.coutinho@pixelmags.com>
2015-11-01 02:48:31 +0000

    MySubscriptions - SQLite base

---
+++ app/src/main/java/com/pixelmags/android/storage/MySubscriptionsDataSet.java
@@ -0,0 +1,18 @@
+package com.pixelmags.android.storage;
+
+import android.content.Context;
+
+/**
+ * Created by austincoutinho on 01/11/15.
+ */
+public class MySubscriptionsDataSet extends BrandedSQLiteHelper {
+
+
+    public MySubscriptionsDataSet(Context context) {
+        super(context);
+    }
+
+
+
+
+}
