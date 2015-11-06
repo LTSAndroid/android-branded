a2545dd92b36544749b7b7ead82be99a3f046b6f
austincoutinho <austin.coutinho@pixelmags.com>
2015-11-06 03:46:14 +0000

    IssueParser changes

--- app/src/main/java/com/pixelmags/android/json/GetIssueParser.java
+++ app/src/main/java/com/pixelmags/android/json/GetIssueParser.java
@@ -14,12 +14,11 @@ import java.util.ArrayList;
  */
 public class GetIssueParser extends JSONParser {
 
-    public Object mData;
-    public ArrayList<Issue> getIssueList;
+    public Issue mIssue;
 
     public GetIssueParser(String Data){
         super(Data);
-        getIssueList = new ArrayList<Issue>();
+        mIssue = new Issue();
     }
 
     public boolean parse(){
@@ -31,42 +30,36 @@ public class GetIssueParser extends JSONParser {
 
             JSONObject unit = baseJSON.getJSONObject("issue");
 
-                Issue issue = new Issue();
-                issue.issueID = unit.getInt("ID");
-                issue.magazineID = unit.getInt("magazine_id");
-                issue.title = unit.getString("title");
-                issue.thumbnailURL = unit.getString("thumbnailURL");
-                issue.issueDate = unit.getString("issueDate");
-                issue.created = unit.getString("created");
-                issue.lastModified = unit.getString("lastModified");
-                issue.media_format = unit.getString("media_format");
+            mIssue.issueID = unit.getInt("ID");
+            mIssue.magazineID = unit.getInt("magazine_id");
+            mIssue.title = unit.getString("title");
+            mIssue.thumbnailURL = unit.getString("thumbnailURL");
+            mIssue.issueDate = unit.getString("issueDate");
+            mIssue.created = unit.getString("created");
+            mIssue.lastModified = unit.getString("lastModified");
+            mIssue.media_format = unit.getString("media_format");
 
-                int pageCount = unit.getInt("pageCount");
-                issue.pageCount = pageCount;
+            int pageCount = unit.getInt("pageCount");
+            mIssue.pageCount = pageCount;
 
-                System.out.println("RETRIEVED ISSUE===" +unit.getString("title"));
+                    System.out.println("RETRIEVED ISSUE===" +unit.getString("title"));
 
-                JSONObject pageManifestArrayData = unit.getJSONObject("page_manifest");
-                for(int pageNo=1; pageNo<=pageCount; pageNo++)
-                {
-                    String pg = String.valueOf(pageNo);
-                    JSONObject pageUnit = pageManifestArrayData.getJSONObject(pg);
+            JSONObject pageManifestArrayData = unit.getJSONObject("page_manifest");
+            for(int pageNo=1; pageNo<=pageCount; pageNo++)
+            {
+                String pg = String.valueOf(pageNo);
+                JSONObject pageUnit = pageManifestArrayData.getJSONObject(pg);
 
+                String pageId = pageUnit.getString("id");
+                String pageMedia = pageUnit.getJSONObject("media").toString();
 
-                    //   pages.add(0,base);
-                    String pageId = pageUnit.getString("id");
-                    String pageMedia = pageUnit.getJSONObject("media").toString();
+                    System.out.println("PAGE ID ==== "+pageId);
+                    // System.out.println("PAGE Media ==== "+pageMedia);
 
-                        System.out.println("PAGE ID ==== "+pageId);
-                        System.out.println("PAGE Media ==== "+pageMedia);
+                Page pageImage = new PageTypeImage(pageNo, pageId, pageMedia);
 
-                    Page pageImage = new PageTypeImage(pageNo, pageId, pageMedia);
-
-                    issue.pages.add(pageImage);
-                }
-
-
-            getIssueList.add(issue);
+                mIssue.pages.add(pageImage);
+            }
 
 
         }catch(Exception e){
