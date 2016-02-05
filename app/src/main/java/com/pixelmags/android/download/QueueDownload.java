4bf68fd0f1702141a13edc6f70fdae7e56627b27
austincoutinho <austin.coutinho@pixelmags.com>
2016-02-05 09:17:48 +0000

    Queue Issue for Download

---
+++ app/src/main/java/com/pixelmags/android/download/QueueDownload.java
@@ -0,0 +1,61 @@
+package com.pixelmags.android.download;
+
+import com.pixelmags.android.datamodels.Issue;
+import com.pixelmags.android.storage.AllDownloadsDataSet;
+import com.pixelmags.android.storage.IssueDataSet;
+import com.pixelmags.android.util.BaseApp;
+
+/**
+ * Created by austincoutinho on 04/02/16.
+ *
+ * This class is used to insert an Issue, etc in the Download Queue
+ *
+ */
+public class QueueDownload {
+
+
+    public QueueDownload(){
+
+    }
+
+    public boolean insertIssueInDownloadQueue(String issueId) {
+
+        try{
+
+            IssueDataSet mDbReader = new IssueDataSet(BaseApp.getContext());
+            Issue mIssue = mDbReader.getIssue(mDbReader.getReadableDatabase(), issueId);
+            mDbReader.close();
+
+            if(mIssue != null){
+
+                AllDownloadsDataSet mDownloadsDbReader = new AllDownloadsDataSet(BaseApp.getContext());
+                boolean result = mDownloadsDbReader.issueDownloadPreChecksAndDownload(mDownloadsDbReader.getWritableDatabase(), mIssue);
+                mDownloadsDbReader.close();
+
+                if(result){
+
+                    //move the issue thumbnail inside the Issue Download Thumbnail folder
+                    DownloadThumbnails.copyThumbnailOfIssueDownloaded(String.valueOf(mIssue.issueID));
+
+                    //TODO :- Remove this thumnail on delete
+
+                }
+
+                return result;
+
+            }
+
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        return false;
+
+    }
+
+
+}
--- app/src/main/java/com/pixelmags/android/pixelmagsapp/ui/IssueDetailsFragment.java
+++ app/src/main/java/com/pixelmags/android/pixelmagsapp/ui/IssueDetailsFragment.java
@@ -3,7 +3,6 @@ package com.pixelmags.android.pixelmagsapp.ui;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -19,17 +18,12 @@ import android.widget.Toast;
 import com.pixelmags.android.api.GetIssue;
 import com.pixelmags.android.api.GetPreviewImages;
 import com.pixelmags.android.comms.Config;
-import com.pixelmags.android.datamodels.Issue;
 import com.pixelmags.android.datamodels.Magazine;
-import com.pixelmags.android.datamodels.PageTypeImage;
 import com.pixelmags.android.datamodels.PreviewImage;
-import com.pixelmags.android.download.DownloadIssue;
-import com.pixelmags.android.download.DownloadIssueThreaded;
 import com.pixelmags.android.download.DownloadPreviewImages;
-import com.pixelmags.android.download.DownloadThumbnails;
+import com.pixelmags.android.download.QueueDownload;
 import com.pixelmags.android.pixelmagsapp.R;
 import com.pixelmags.android.storage.AllIssuesDataSet;
-import com.pixelmags.android.storage.IssueDataSet;
 import com.pixelmags.android.util.BaseApp;
 
 import java.io.File;
@@ -157,45 +151,13 @@ public class IssueDetailsFragment extends Fragment {
 
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-     */
 
     private boolean startIssueDownload(String issueId){
 
-        // Test the saved output
-        System.out.println("<<< issueId :: "+ issueId +" >>>");
+        QueueDownload queueIssue = new QueueDownload();
+        return queueIssue.insertIssueInDownloadQueue(issueId);
 
-        IssueDataSet mDbReader = new IssueDataSet(BaseApp.getContext());
-        Issue issueData = mDbReader.getIssue(mDbReader.getReadableDatabase(), issueId);
-        mDbReader.close();
-
-        System.out.println("<<< STARTING ISSUE DOWNLOAD "+ issueData.issueID +" >>>");
-
-        if(issueData != null){
-        // Download via queueing
-            //DownloadIssue downloadIssue = new DownloadIssue(issueData);
-            //downloadIssue.initDownload();
-
-            return DownloadIssueThreaded.DownloadIssuePages(issueData);
-
-        }
-
-        return false;
     }
-
 
     /**
      *
