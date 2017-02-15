ce4d2560177245cdf8a7214a6dd6d54a70e94667
anniekummari <annie.kummari@pixelmags.com>
2017-02-16 00:00:59 +0530

    1. Missed one file

---
+++ app/src/main/java/com/pixelmags/android/bean/DataTransferInterface.java
@@ -0,0 +1,11 @@
+package com.pixelmags.android.bean;
+
+/**
+ * Created by likith on 2/10/17.
+ */
+public class DataTransferInterface {
+
+    public static int count;
+    public static int issueId;
+
+}
